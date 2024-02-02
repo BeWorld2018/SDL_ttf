fe74c2ed7a8e77dee307fb60f049ddfc7f63b25c
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2024-02-02 11:12:17 +0100

    MorphOS; update version

--- morphos/TTF_version.h
+++ morphos/TTF_version.h
@@ -1,5 +1,5 @@
 #define	str(s) #s
 #define	xstr(s) str(s)
 #define	VERSION	53
-#define	REVISION	6
+#define	REVISION	7
 #define	VERSTAG	"\0$VER: sdl2_ttf.library " xstr(VERSION) "." xstr(REVISION) " (" __AMIGADATE__ ") © Bruno Peloille, Szilárd Biró, Ilkka Lehtoranta "
